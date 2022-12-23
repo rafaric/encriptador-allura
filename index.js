db4091c21a0b55cc8a9d3b7b70ab176cd2c1737f
rafaric <rafaelstrongoli@gmail.com>
2022-12-23 12:22:35 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+const encriptar = () =>{
+  const textoingresado = document.getElementById('ingresado').value;
+  const nuevotexto = textoingresado.replaceAll('e','enter').replaceAll('i','imes').replaceAll('a','ai').replaceAll('o','ober').replaceAll('u','ufat')
+
+  document.getElementById('devuelve').innerHTML = nuevotexto;
+  document.getElementById('devuelve').style.display = "block";
+  document.getElementById('copiar').style.display = "block";
+  document.getElementById('logo').style.display = "none";
+  document.getElementById('h2').style.display = "none";
+  document.getElementById('mensaje').style.display = "none";
+
+}
+
+const copiar = () =>{
+  const textocopiado = document.getElementById('devuelve').textContent;
+  navigator.clipboard.writeText(textocopiado);
+}
+
+const desencriptar = () =>{
+  const textoingresado = document.getElementById('ingresado').value;
+  const nuevotexto = textoingresado.replaceAll('enter','e').replaceAll('imes','i').replaceAll('ai','a').replaceAll('ober','o').replaceAll('ufat','u');
+  document.getElementById('devuelve').innerHTML = nuevotexto;
+  document.getElementById('devuelve').style.display = "block";
+  document.getElementById('copiar').style.display = "block";
+  document.getElementById('logo').style.display = "none";
+  document.getElementById('h2').style.display = "none";
+  document.getElementById('mensaje').style.display = "none";
+}
